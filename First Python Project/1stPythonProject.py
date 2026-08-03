6b2f89533947cee14613504f7c6b3f9b7c319899
GillyNombrado <gillian.nombrado0@gmail.com>
2026-08-03 16:31:17 +0800

    First Activity

---
+++ First Python Project/1stPythonProject.py
@@ -0,0 +1,5 @@
+print("""Nombrado, Gillian Threse G.
+Bachelor of Science in Computer Science majoring in Intelligent Systems
+3A
+Favorite AI Technology: Chatbots
+Future AI Project: Chatbots for mental health support""")
